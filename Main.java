0e10b41c72397bbfebd6027d134e4d338295576f
Aryaa-mahi <agastyaa814@gmail.com>
2024-12-29 23:30:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+class Person {
+    //encapsulation
+    private String name;
+    private int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Public getter and setter 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Age must be positive.");
+        }
+    }
+
+    // Display person details
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a new Person object
+        Person person = new Person("Mahi", 25);
+        person.displayDetails();
+
+        // Modify details using setter methods
+        person.setName("Anya");
+        person.setAge(30);
+        person.displayDetails();
+    }
+}
