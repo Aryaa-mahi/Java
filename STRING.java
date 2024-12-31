8356cfa0f16967c3854da8a207fe455c8e03c19e
Aryaa-mahi <agastyaa814@gmail.com>
2024-12-31 22:04:47 +0530

    Add files via upload

---
+++ STRING.java
@@ -0,0 +1,14 @@
+public class STRING{
+    public static void main(String[] args) {
+        //creating java string 
+        String s1 = "HAPPY-";
+        String s2 = "NEW-";
+        s1 = s1.concat(s2);
+        System.out.println(s1);
+        String s4 = "YEAR'2025'";
+        String s5 = s1.concat(s4);
+        System.out.println(s5);
+
+        
+    }
+}
