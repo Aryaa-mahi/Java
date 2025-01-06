83437bf5b0a278dc814a9c7a21a8060705790a2d
Aryaa-mahi <agastyaa814@gmail.com>
2025-01-06 23:41:10 +0530

    Add files via upload

---
+++ Fruits.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Fruits {
+    public static void main(String[] args) {
+        Collection<String> c = new ArrayList<>();
+        
+        c.add("Apple");
+        c.add("");
+        c.add("Orange");
+        c.add("Mango");
+        System.out.println("Collection: " + c);
+    }
+}
